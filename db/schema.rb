1b5dbd57d063f09dbfc4cbe3b2de908e8670657c
Michael Li <mike830@gmail.com>
2018-11-12 19:28:57 +0900

    initial model migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_12_101421) do
+ActiveRecord::Schema.define(version: 2018_11_12_101738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
-    t.date "date"
     t.bigint "venue_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_bookings_on_user_id"
     t.index ["venue_id"], name: "index_bookings_on_venue_id"
   end
 
@@ -40,9 +41,13 @@ ActiveRecord::Schema.define(version: 2018_11_12_101421) do
     t.string "address"
     t.integer "price"
     t.string "photo"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_venues_on_user_id"
   end
 
+  add_foreign_key "bookings", "users"
   add_foreign_key "bookings", "venues"
+  add_foreign_key "venues", "users"
 end
