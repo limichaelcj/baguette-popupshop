edc9f1312b9da192a955eaf851f7c9b024b98b50
Michael Li <mike830@gmail.com>
2018-11-13 12:22:41 +0900

    Change booking model name to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,4 @@
-class Booking < ApplicationRecord
+class Event < ApplicationRecord
   belongs_to :venue
   belongs_to :artist, class_name: "User", foreign_key: 'user_id'
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,7 +1,7 @@
 class Venue < ApplicationRecord
   belongs_to :host, class_name: "User", foreign_key: 'user_id'
-  has_many :bookings, dependent: :destroy
-  has_many :artists, through: :bookings, source: :user
+  has_many :events, dependent: :destroy
+  has_many :artists, through: :events, source: :user
 
   mount_uploader :photo, PhotoUploader
 end
