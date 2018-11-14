5b91c887221be74f5968ec4dfa9ddcff9d31fd47
Michael Li <mike830@gmail.com>
2018-11-14 12:06:10 +0900

    sync calendar with event bookings data

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -2,6 +2,16 @@ import flatpickr from "flatpickr"
 import "flatpickr/dist/themes/dark.css"
 // import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 
-flatpickr(".datepicker", {
-  altInput: true
+const today = new Date();
+const year = today.getFullYear();
+const month = today.getMonth();
+const day = today.getDate();
+
+flatpickr("#datepicker", {
+  altInput: true,
+  defaultDate: "today",
+  minDate: "today",
+  maxDate: new Date(year, month + 1, day),
+  disable: gon.booked_events,
+  inline: true
 })
