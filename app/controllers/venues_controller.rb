92e5280a556c6638f683857097685698a6d80577
Michael Li <mike830@gmail.com>
2018-11-15 13:49:19 +0900

    refactor controllers for view data

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -3,22 +3,16 @@ class VenuesController < ApplicationController
     @venues = Venue.all
     @venues = Venue.where.not(latitude: nil, longitude: nil)
     @markers = @venues.map do |venue|
-      {
-        lng: venue.longitude,
-        lat: venue.latitude
-      }
-   end
+      { lng: venue.longitude, lat: venue.latitude }
+    end
   end
-   def show
-     @venue = Venue.find(params[:id])
-     @event = Event.new
-     gon.booked_events = Event.where(venue: @venue).map(&:date)
-     gon.venue_name = @venue.name
+
+  def show
+    @venue = Venue.find(params[:id])
+    @event = Event.new
+    gon.booked_events = Event.where(venue: @venue).map(&:date)
+    gon.venue_name = @venue.name
     # the markers array only has one marker
-     @markers = []
-     @markers << {
-       lng: @venue.longitude,
-       lat: @venue.latitude
-     }
-   end
+    @markers = [{ lng: @venue.longitude, lat: @venue.latitude }]
+  end
 end
